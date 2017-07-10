f7a1ade255ffb1c96203516d2dca20fd034f8026
JosepSampe <jsampe@gmail.com>
2017-07-10 11:49:31 +0200

    Dockerfile updated

--- Engine/compute/service/init_container.sh
+++ Engine/compute/service/init_container.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-if [ $1 = "debug" ]; then
-    $2
-else
-	service redis-server restart
-    swift-init main restart
-	python /home/zion/zion_framework/Engine/compute/service/blackeagle_service.py
-fi
+service redis-server restart
+swift-init main restart
+python /home/zion/zion_framework/Engine/compute/service/blackeagle_service.py
